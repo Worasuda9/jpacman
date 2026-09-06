1e1a263f158fbe65b72b7d4d1c5d30c5a7a04c6d
Worasuda9 <worasuda.n@gmail.com>
2026-09-06 14:14:32 +0700

    add more board test cases

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,13 +1,14 @@
 package nl.tudelft.jpacman.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
 
     @Test
-    void validBoard() {
+    void validBoardSquareAt() {
         Square[][] grid = {
             { new BasicSquare() }
         };
@@ -22,7 +23,55 @@ class BoardTest {
         Square[][] grid = {
             { null }
         };
+
+        assertThrows(AssertionError.class, () -> new Board(grid));
+    }
+
+    @Test
+    void testGetWidth() {
+        Square[][] grid = {
+            { new BasicSquare() },
+            { new BasicSquare() }
+        };
+
         Board board = new Board(grid);
-        assertThat(board.squareAt(0, 0)).isNull();
+
+        assertThat(board.getWidth()).isEqualTo(2);
+    }
+
+    @Test
+    void testGetHeight() {
+        Square[][] grid = {
+            { new BasicSquare(), new BasicSquare() }
+        };
+
+        Board board = new Board(grid);
+
+        assertThat(board.getHeight()).isEqualTo(2);
+    }
+
+    @Test
+    void testWithinBorders() {
+        Square[][] grid = {
+            { new BasicSquare() }
+        };
+
+        Board board = new Board(grid);
+
+        assertThat(board.withinBorders(0, 0)).isTrue();
+        assertThat(board.withinBorders(1, 0)).isFalse();
+        assertThat(board.withinBorders(0, 1)).isFalse();
+        assertThat(board.withinBorders(-1, 0)).isFalse();
+    }
+
+    @Test
+    void testSquareAtOutsideBoard() {
+        Square[][] grid = {
+            { new BasicSquare() }
+        };
+
+        Board board = new Board(grid);
+
+        assertThrows(AssertionError.class, () -> board.squareAt(1, 0));
     }
 }
